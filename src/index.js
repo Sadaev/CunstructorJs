f1572a77f35a4b9d414dbef5df773c2554693e12
Садаев Шынгыс <sadaevsh@gmail.com>
2021-02-26 23:45:14 +0600

    day 4 i hope

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import {model} from './model'
-import { title, text, columns, image } from './templates'
+import {templates} from './templates'
 import './styles/main.css'
 
 const $site = document.querySelector('#site')
 
+
+
 model.forEach(block => {
-    let html = ''
-    if(block.type === 'title'){
-        html = title(block)
-    }else if(block.type === 'text'){
-        html = text(block)
-    }else if(block.type === 'columns'){
-        html = columns(block)        
-    }else if(block.type === 'image'){
-        html = image(block)
+    const toHtml = templates[block.type]
+    if(toHtml){
+        $site.insertAdjacentHTML('beforeend', toHtml(block))
     }
-    $site.insertAdjacentHTML('beforeend', html)
 })
 
